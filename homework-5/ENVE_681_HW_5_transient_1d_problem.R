535b6db701b7542364ca685cb7aa34a2b6e4711f
Nicola Horscroft <nicola.horscroft@phila.gov>
2017-11-13 16:17:42 -0500

    added input variables

--- homework-5/ENVE_681_HW_5_transient_1d_problem.R
+++ homework-5/ENVE_681_HW_5_transient_1d_problem.R
@@ -8,3 +8,14 @@ library(plotly)
 
 
 # Define input variables-------------------------------------------------------
+
+# l = length (m)
+l = 400
+
+# d = depth (m)
+
+# ks = hydraulic conductivity (m/day)
+ks = 5
+
+# sy = specific yield
+sy = 0.8
